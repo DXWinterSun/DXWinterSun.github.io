716a4bdf7d0c4c26f21e892749e8e767fd6a0d43
jixi <jixi@pinduoduo.com>
2022-11-27 21:47:33 +0800

    update: 尝试修复category的问题

--- _plugins/category-generator.rb
+++ _plugins/category-generator.rb
@@ -8,7 +8,7 @@ module Jekyll
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'category-page.html')
+      self.read_yaml(File.join(base, '_layouts'), 'category.html')
       self.data['category'] = category
       self.data['title'] = category
     end
@@ -18,7 +18,7 @@ module Jekyll
     safe true
 
     def generate(site)
-      if site.layouts.key? 'category-page'
+      if site.layouts.key? 'category'
         dir = site.config['category_dir'] || 'category'
         site.categories.each_key do |category|
           category_dir = File.join(dir, Utils.slugify(category))
